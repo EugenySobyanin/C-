e3f66efe22929e814e1e753c46703bc07a9e67ff
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-11-29 12:58:41 +0300

    авторизация и регистрация.

---
+++ 5. JS/DEV/1. Authorization_&_Registration/js/auth.js
@@ -0,0 +1,6 @@
+let inputs = document.querySelectorAll(".form-input");
+submint_button = document.getElementsByClassName("submit-button")[0];
+
+submint_button.onclick = function(){
+    console.log("Работает!");
+}
