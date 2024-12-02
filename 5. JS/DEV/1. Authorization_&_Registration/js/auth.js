f8069e5f3d6657aa9a2b3c1eec9be79eb4253d70
Евгений Собянин <ea.sobyanin2018@gmail.com>
2024-12-02 09:50:21 +0300

    Какие-то файлы с ноутбука.

--- 5. JS/DEV/1. Authorization_&_Registration/js/auth.js
+++ 5. JS/DEV/1. Authorization_&_Registration/js/auth.js
@@ -1,6 +1,23 @@
 let inputs = document.querySelectorAll(".form-input");
-submint_button = document.getElementsByClassName("submit-button")[0];
+let submint_button = document.getElementsByClassName("submit-button")[0];
+let username_warning = document.getElementsByClassName("warning")[0];
+let auth_form_warning = document.getElementsByClassName("warning")[1];
 
-submint_button.onclick = function(){
-    console.log("Работает!");
+submint_button.onclick = function(event){
+    event.preventDefault();
+    let username = inputs[0].value;
+    let password = inputs[1].value;
+
+    if (username == "Bob"){
+        username_warning.innerHTML = "Бобу нельзя заходить на сайт!";
+    }
+
+    if (username === password){
+        auth_form_warning.innerHTML = "username и пароль не могут совпадать.";
+    }
+
+    if (username == ""){
+        inputs[0].style.border = "3px solid red";
+    }
+
 }
